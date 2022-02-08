c92932e5e504e6ba6fd0f601eee800ca152807ff
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-02-08 09:11:18 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -7,7 +7,7 @@ void PrintOnConsole( char message[]){
 }
 
 int TestBatteryTempIsOk(float temperature){
-    if(temperature < 0 || temperature > 45) {
+    if(temperature < TEMP_MIN_LIMIT || temperature > TEMP_MAX_LIMIT) {
       PrintOnConsole("Temperature");
       return 0;
     }
@@ -15,7 +15,7 @@ int TestBatteryTempIsOk(float temperature){
 }
 
 int TestBatterySocIsOk(float soc){
-    if(soc < 20 || soc > 80) {
+    if(soc < SOC_MIN_LIMIT || soc > SOC_MAX_LIMIT) {
       PrintOnConsole("State of Charge");
       return 0;
     }
@@ -23,21 +23,26 @@ int TestBatterySocIsOk(float soc){
 }
 
 int TestBatteryChargeRateIsOk(float chargeRate){
-    if(chargeRate > 0.8) {
+    if(chargeRate < CHARGERATE_MIN_LIMIT || chargeRate > CHARGERATE_MAX_LIMIT) {
       PrintOnConsole("Charge Rate");
       return 0;
     }
   return 1;
 }
 
-void AssertBatteryIsOk(float temperature, float soc, float chargeRate) {
-  assert(TestBatteryTempIsOk(temperature) ==1);
-  assert(TestBatterySocIsOk(soc)==1);
-  assert(TestBatteryChargeRateIsOk(chargeRate)==1);
+void AssertBatteryIsOk(BatteryTestData testdata , int result) {
+  int output;
+  output =  TestBatteryTempIsOk(testdata.Temperature);
+  output &= TestBatterySocIsOk(testdata.StateOfCharge);
+  output &= TestBatteryChargeRateIsOk(testdata.ChargeRate);
+  assert( output == result);
 }
 
  
 int main() {
-  AssertBatteryIsOk(25, 70, 0.7);
-  AssertBatteryIsOk(50, 85, 0);
+  BatteryTestData testdata[];
+  testdata[0] = { 20,70,0.7};
+  testdata[1] = {50,85,0};
+  AssertBatteryIsOk(testdata[0],1);
+  AssertBatteryIsOk(testdata[1],0);
 }
