d0f4bf0e7584cda46a86eed1f4205413fd01369c
AbhijeetArjun-Shinde <91106940+AbhijeetArjun-Shinde@users.noreply.github.com>
2022-02-14 11:10:17 +0530

    Update checker.h

--- checker.h
+++ checker.h
@@ -14,7 +14,28 @@ typedef struct{
   float ChargeRate;
 } BatteryTestData;
 
+tyedef struct{
+  float lowlimit;
+  float highlimit;
+}RANGE;
 
+RANGE TemperatureLowBreach = {0,TEMP_MIN_LIMIT};
+RANGE TemperatureLowWarning = {TEMP_MIN_LIMIT, (TEMP_MIN_LIMIT + 0.05 * TEMP_MAX_LIMIT)}; //5%
+RANGE TemperatureNormal = {(TEMP_MIN_LIMIT + 0.05 * TEMP_MAX_LIMIT) , (TEMP_MAX_LIMIT - 0.05 * TEMP_MAX_LIMIT )};
+RANGE TemperatureHighWarning = {(TEMP_MAX_LIMIT - 0.05 * TEMP_MAX_LIMIT ), TEMP_MAX_LIMIT}; //5%
+RANGE TemperatureHighBreach = {TEMP_MAX_LIMIT,100};
+
+RANGE SOCLowBreach = {0,SOC_MIN_LIMIT};
+RANGE SOCLowWarning = {SOC_MIN_LIMIT, (SOC_MIN_LIMIT + 0.05 * SOC_MAX_LIMIT)}; //5%
+RANGE SOCNormal = {(SOC_MIN_LIMIT + 0.05 * SOC_MAX_LIMIT) , (SOC_MAX_LIMIT - 0.05 * SOC_MAX_LIMIT )};
+RANGE SOCHighWarning = {(SOC_MAX_LIMIT - 0.05 * SOC_MAX_LIMIT ), SOC_MAX_LIMIT}; //5%
+RANGE SOCHighBreach = {SOC_MAX_LIMIT,100};
+
+RANGE ChargeRateLowBreach = {0,CHARGERATE_MIN_LIMIT};
+RANGE ChargeRateLowWarning = {CHARGERATE_MIN_LIMIT, (CHARGERATE_MIN_LIMIT + 0.05 * CHARGERATE_MAX_LIMIT)}; //5%
+RANGE ChargeRateNormal = {(CHARGERATE_MIN_LIMIT + 0.05 * CHARGERATE_MAX_LIMIT) , (CHARGERATE_MAX_LIMIT - 0.05 * CHARGERATE_MAX_LIMIT )};
+RANGE ChargeRateHighWarning = {(CHARGERATE_MAX_LIMIT - 0.05 * CHARGERATE_MAX_LIMIT ), CHARGERATE_MAX_LIMIT}; //5%
+RANGE ChargeRateHighBreach = {CHARGERATE_MAX_LIMIT,100};
 
 void PrintOnConsole( char message[]);
 
